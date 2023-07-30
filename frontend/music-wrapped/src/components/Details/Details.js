6dec6be4a28c671ff72d8f68a37edff87691a8cf
adii21-ux <adityasangave2112@gmail.com>
2023-07-30 22:44:06 +0530

    Added progress bar

--- frontend/music-wrapped/src/components/Details/Details.js
+++ frontend/music-wrapped/src/components/Details/Details.js
@@ -7,6 +7,7 @@ export default function Details() {
   const [trackInfo, setTrackInfo] = useState();
   const [artistInfo, setArtistInfo] = useState();
   const [params, setParams] = useSearchParams();
+  const [loading, setLoading] = useState(true);
 
   const userTrackInfo = () => {
     axios.get('/info/get-tracks', {
@@ -19,10 +20,12 @@ export default function Details() {
         return response.data;
       })
       .then((data) => {
+        setLoading(false)
         console.log(data);
         setTrackInfo(data);
       })
       .catch((error) => {
+        setLoading(false)
         console.error('Error:', error.message);
       });
   }
@@ -38,10 +41,12 @@ export default function Details() {
         return response.data;
       })
       .then((data) => {
+        setLoading(false)
         setArtistInfo(data);
         console.log(data);
       })
       .catch((error) => {
+        setLoading(false)
         console.error('Error:', error.message);
       });
   }
@@ -57,30 +62,38 @@ export default function Details() {
     return (
       <div className='container'>
         <h1 className='heading tracks'>Your Top Songs</h1>
-        <div className='trackContainer'>
-          {trackInfo?.items.slice(0, 5).map((item) => (
-            <div key={item.id} className='trackItem card'>
-              <img src={item.album.images[0].url} alt={item.name} className='trackImage' />
-              <div className='trackDetails'>
-                <h3 className='trackName'>{item.name}</h3>
-                <p className='artistName'>{item.artists[0].name}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className='artistInfo'>
-          <h1 className='heading tracks'>Your Top Artists</h1>
-          <div className='artistContainer'>
-            {artistInfo?.items.slice(0, 5).map((artist) => (
-              <div key={artist.id} className='artistItem'>
-                <img src={artist.images[0].url} alt={artist.name} className='artistImage' />
-                <div className='artistDetails'>
-                  <h3 className='artistName1'>{artist.name}</h3>
-                  {/* <p className='artistGenres'>Genres: {artist.genres.join(', ')}</p> */}
+        {loading ? (
+          <div className="progress-bar">Loading...</div>
+        ) : (
+          <div className='trackContainer'>
+            {trackInfo?.items.slice(0, 5).map((item) => (
+              <div key={item.id} className='trackItem card'>
+                <img src={item.album.images[0].url} alt={item.name} className='trackImage' />
+                <div className='trackDetails'>
+                  <h3 className='trackName'>{item.name}</h3>
+                  <p className='artistName'>{item.artists[0].name}</p>
                 </div>
               </div>
             ))}
           </div>
+        )}
+        <div className='artistInfo'>
+          <h1 className='heading tracks'>Your Top Artists</h1>
+          {loading ? (
+            <div className="progress-bar">Loading...</div>
+          ) : (
+            <div className='artistContainer'>
+              {artistInfo?.items.slice(0, 5).map((artist) => (
+                <div key={artist.id} className='artistItem'>
+                  <img src={artist.images[0].url} alt={artist.name} className='artistImage' />
+                  <div className='artistDetails'>
+                    <h3 className='artistName1'>{artist.name}</h3>
+                    {/* <p className='artistGenres'>Genres: {artist.genres.join(', ')}</p> */}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
