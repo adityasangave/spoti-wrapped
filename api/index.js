94a7554a044cc951cc62605ae51573c760813c66
adii21-ux <adityasangave2112@gmail.com>
2023-08-27 00:42:20 +0530

    cors updated

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: "https://music-wrapped-main.vercel.app/", //"http://localhost:3000",
+    origin: "https://music-wrapped-main.vercel.app", //"http://localhost:3000",
     methods:["GET"]
 }))
 
